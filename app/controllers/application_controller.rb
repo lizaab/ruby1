0880f2f7993db3267b784a3ab2b00110f6ea97a6
lizaab <liza_banks@hotmail.com>
2018-02-18 23:10:58 +0000

    Improve the README file

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+    def hello
+    render html: "¡Hola mundo!"
+    end
+end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+    def goodbye
+    render html: "test, world!"
+    end
+end
