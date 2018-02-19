8805c6cc1d9b0411503969634207f2e20163a94a
lizaab <liza_banks@hotmail.com>
2018-02-19 03:37:13 +0000

    add edited controller

--- application_controller.rb
+++ application_controller.rb
@@ -7,6 +7,6 @@ end
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
     def goodbye
-    render html: "Goodbye, world!"
+    render html: "goodbye, world!"
     end
 end
